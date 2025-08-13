ba0339408c94689159ebf1f2b6a314a5b18e1ebb
agusescobbar <panmmnda@gmail.com>
2025-08-13 16:57:41 -0300

    cambios css, funciones listas

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const { act } = require("react");
-
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
 //funcion agregar amigo
 function agregarAmigo(){
-    const inputAmigo = document.getElementById("Amigo");
+    const inputAmigo = document.getElementById('amigo');
     const nombreAmigo = inputAmigo.value.trim();
-
 
     if (nombreAmigo === "") {
         alert("Por favor, ingresa un nombre válido.");
@@ -26,8 +23,28 @@ function agregarAmigo(){
     actualizarLista();
 }
 
-
 // funcion para actualizar la lista de amigos
+function actualizarLista() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ""; 
 
+    for(let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
 
 //funcion para sortear los amigos
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Necesitas al menos dos amigos para hacer un sorteo.");
+        return;
+    }
+
+    const indiceRandom = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceRandom];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>El amigo sorteado es: ${amigoSorteado}</li>`;
+}
